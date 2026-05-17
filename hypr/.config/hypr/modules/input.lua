1098bba40563febfa6c553bd3efa6b0e44cea2db
Alexander Levy <ingalexanderlevy@gmail.com>
2026-05-17 19:47:45 -0400

    Refined hyprland config & fixed clipboard history

--- hypr/.config/hypr/modules/input.lua
+++ hypr/.config/hypr/modules/input.lua
@@ -1,21 +1,20 @@
-------------------------------------------------------------------------------
--- Levy's Hyprland input
-------------------------------------------------------------------------------
+-- ===========================================================================
+-- Levy's Hyprland Input 
+-- ===========================================================================
 hl.config({
     input = {
         -- Keyboard
         kb_layout    = "us",
         repeat_rate  = 35,
         repeat_delay = 200,
+
         -- Mouse & Touchpad
         sensitivity  = 0,
         follow_mouse = 1,
-        touchpad = {
-            natural_scroll = true,
-        },
+        touchpad = { natural_scroll = true },
     },
 })
 
 -- Gestures
-hl.gesture({ fingers = 3, direction = "horizontal",action = "workspace" })
+hl.gesture({ fingers = 3, direction = "horizontal", action = "workspace" })
 
--- hypr/.config/hypr/modules/keybinds.lua
+++ hypr/.config/hypr/modules/keybinds.lua
@@ -1,6 +1,6 @@
-------------------------------------------------------------------------------
+-- ===========================================================================
 -- Levy's Hyprland Programs & Keybinds
-------------------------------------------------------------------------------
+-- ===========================================================================
 -- Default Programs 
 local menu         = "wofi"
 local browser      = "firefox"
@@ -18,37 +18,36 @@ local closeSession = "command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown ||
 -- Utilities
 local colorPicker = "hyprpicker -n -a"
 local screenSaver = terminal .. "-e cmatrix -sa -u 3 -C cyan"
-local clipBoard   = "cliphist list | wofi --dmenu --pre-display-cmd 'echo \'%s\' | cut -f 2' | cliphist decode | wl-copy"
+local clipBoard   = 'cliphist list | wofi --dmenu --pre-display-cmd "echo \'%s\' | cut -f 2" | cliphist decode | wl-copy'
 
-------------------------------------------------------------------------------
+-- ===========================================================================
 -- Keybinds
-------------------------------------------------------------------------------
+-- ===========================================================================
 local mainMod = "SUPER"
 
--- Launch & Close Programs
+-- App launcher, terminal & close windows
 hl.bind("ALT  + SPACE", hl.dsp.exec_cmd(menu))
 hl.bind("CTRL + SPACE", hl.dsp.exec_cmd(terminal))
-hl.bind(mainMod .. " + B", hl.dsp.exec_cmd(browser))
-hl.bind(mainMod .. " + G", hl.dsp.exec_cmd(gameLauncher))
 hl.bind(mainMod .. " + Q", hl.dsp.window.close())
 
--- Shell Elements
-hl.bind(mainMod .. " + Z", hl.dsp.exec_cmd(nitroSense))
+-- Programs
+hl.bind(mainMod .. " + B", hl.dsp.exec_cmd(browser))
 hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
-hl.bind(mainMod .. " + N", hl.dsp.exec_cmd(notification))
+hl.bind(mainMod .. " + G", hl.dsp.exec_cmd(gameLauncher))
+hl.bind(mainMod .. " + Z", hl.dsp.exec_cmd(nitroSense))
 hl.bind("SUPER + SHIFT + ESCAPE", hl.dsp.exec_cmd(taskManager))
 
 -- System Utilities 
+hl.bind(mainMod .. " + V", hl.dsp.exec_cmd(clipBoard))
+hl.bind(mainMod .. " + N", hl.dsp.exec_cmd(notification))
 hl.bind(mainMod .. " + P", hl.dsp.exec_cmd(colorPicker))
 hl.bind(mainMod .. " + S", hl.dsp.exec_cmd(screenSaver))
-hl.bind(mainMod .. " + V", hl.dsp.exec_cmd(clipBoard))
 
 -- Session binds (logout & exit Hyprland)
 hl.bind(mainMod .. " + L", hl.dsp.exec_cmd(lockScreen))
 hl.bind(mainMod .. " + M", hl.dsp.exec_cmd(closeSession))
 
-
--- Change Window Mode: Tiled, Floating, Fullscreen
+-- Change window Mode: Tiled, Floating, Fullscreen
 hl.bind(mainMod .. " + F", hl.dsp.window.fullscreen({ action = "toggle" }))
 hl.bind(mainMod .. " + T", hl.dsp.window.float({ action = "toggle" }))
 
@@ -57,6 +56,9 @@ hl.bind(mainMod .. " + K", hl.dsp.layout("swapcol r"))
 hl.bind(mainMod .. " + J", hl.dsp.layout("swapcol l"))
 hl.bind(mainMod .. " + H", hl.dsp.layout("colresize +conf"))
 
+-- Move/resize windows
+hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(),   { mouse = true })
+hl.bind(mainMod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
 
 -- Move window focus 
 hl.bind(mainMod .. " + up",    hl.dsp.focus({ direction = "up" }))
@@ -64,30 +66,16 @@ hl.bind(mainMod .. " + down",  hl.dsp.focus({ direction = "down" }))
 hl.bind(mainMod .. " + left",  hl.dsp.focus({ direction = "left" }))
 hl.bind(mainMod .. " + right", hl.dsp.focus({ direction = "right" }))
 
--- Switch workspaces with mainMod + [0-9]
--- Move window to workspace with mainMod + SHIFT + [0-9]
+-- Switch workspaces & move windows around workspaces 
 for i = 1, 5 do
-    local key = i % 10 -- 10 maps to key 0
-    hl.bind(mainMod .. " + " .. key,        hl.dsp.focus({ workspace = i}))
-    hl.bind(mainMod .. " + CTRL + " .. key, hl.dsp.window.move({ workspace = i }))
+    hl.bind(mainMod .. " + " .. i, hl.dsp.focus({ workspace = i}))
+    hl.bind(mainMod .. " + CTRL + " .. i, hl.dsp.window.move({ workspace = i }))
+    hl.bind(mainMod .. " + SHIFT + " .. i, hl.dsp.window.move({ workspace = i, follow = false }))
 end
 
 -- Move through existing workspaces
 hl.bind("CTRL + SUPER + left",  hl.dsp.focus({ workspace = "e-1" }))
 hl.bind("CTRL + SUPER + right", hl.dsp.focus({ workspace = "e+1" }))
-
--- Move/resize windows with mainMod + LMB/RMB and dragging
-hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(),   { mouse = true })
-hl.bind(mainMod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
-
-
--- Laptop multimedia keys for volume and screen brightness
-hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"), { locked = true, repeating = true })
-hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),      { locked = true, repeating = true })
-hl.bind("XF86AudioMute",        hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),     { locked = true, repeating = true })
-hl.bind("XF86AudioMicMute",     hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),   { locked = true, repeating = true })
-hl.bind("XF86MonBrightnessUp",  hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"),                  { locked = true, repeating = true })
-hl.bind("XF86MonBrightnessDown",hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"),                  { locked = true, repeating = true })
 
 -- Laptop multimedia keys for play, pause, fowards and reverse
 hl.bind("XF86AudioNext",  hl.dsp.exec_cmd("playerctl next"),       { locked = true })
@@ -95,12 +83,11 @@ hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = tr
 hl.bind("XF86AudioPlay",  hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPrev",  hl.dsp.exec_cmd("playerctl previous"),   { locked = true })
 
--- Unused [Migth be helpful later? maybe]
--- Example special workspace (scratchpad)
--- hl.bind(mainMod .. " + S",         hl.dsp.workspace.toggle_special("magic"))
--- hl.bind(mainMod .. " + SHIFT + S", hl.dsp.window.move({ workspace = "special:magic" }))
-
--- dwindle
--- hl.bind(mainMod .. " + J", hl.dsp.layout("togglesplit"))    -- dwindle only
--- hl.bind(mainMod .. " + P", hl.dsp.window.pseudo())
+-- Laptop multimedia keys for volume and screen brightness
+hl.bind("XF86MonBrightnessUp",  hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"),                  { locked = true, repeating = true })
+hl.bind("XF86MonBrightnessDown",hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"),                  { locked = true, repeating = true })
+hl.bind("XF86AudioMute",        hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),     { locked = true, repeating = true })
+hl.bind("XF86AudioMicMute",     hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),   { locked = true, repeating = true })
+hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),      { locked = true, repeating = true })
+hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"), { locked = true, repeating = true })
 
