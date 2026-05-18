dd9594d6539102d260fe62728b72e2e0d7f1485b
Alexander Levy <ingalexanderlevy@gmail.com>
2026-05-17 20:38:09 -0400

    Changed fish's colorscheme to eldritch & modified prompt

--- fish/.config/fish/conf.d/fish_frozen_theme.fish
+++ fish/.config/fish/conf.d/fish_frozen_theme.fish
@@ -5,41 +5,41 @@
 #     man fish-interactive | less +/^SYNTAX.HIGHLIGHTING
 # for appropriate commands to add to ~/.config/fish/config.fish instead.
 
-set --global fish_color_autosuggestion af0000
-set --global fish_color_cancel --reverse
-set --global fish_color_command FF9400
-set --global fish_color_comment A63100
-set --global fish_color_cwd green
-set --global fish_color_cwd_root red
-set --global fish_color_end FF4C00
-set --global fish_color_error FFDD73
-set --global fish_color_escape 00a6b2
+set --global fish_color_autosuggestion 7081d0
+set --global fish_color_cancel f16c75 --reverse
+set --global fish_color_command 37f499
+set --global fish_color_comment 7081d0
+set --global fish_color_cwd a48cf2
+set --global fish_color_cwd_root f265b5
+set --global fish_color_end a48cf2
+set --global fish_color_error f16c75
+set --global fish_color_escape a48cf2
 set --global fish_color_gray 
 set --global fish_color_history_current --bold
-set --global fish_color_host normal
-set --global fish_color_host_remote yellow
-set --global fish_color_keyword 
-set --global fish_color_normal normal
-set --global fish_color_operator 00a6b2
-set --global fish_color_option 
-set --global fish_color_param FFC000
-set --global fish_color_quote BF9C30
-set --global fish_color_redirection BF5B30
-set --global fish_color_search_match white --bold --background=brblack
-set --global fish_color_selection white --bold --background=brblack
-set --global fish_color_status red
-set --global fish_color_user brgreen
+set --global fish_color_host 04d1f9
+set --global fish_color_host_remote 04d1f9
+set --global fish_color_keyword a48cf2
+set --global fish_color_normal ebfafa
+set --global fish_color_operator a48cf2
+set --global fish_color_option f7c67f
+set --global fish_color_param 04d1f9
+set --global fish_color_quote f1fc79
+set --global fish_color_redirection ebfafa
+set --global fish_color_search_match --background=76639e
+set --global fish_color_selection --background=76639e
+set --global fish_color_status f16c75
+set --global fish_color_user 37f499
 set --global fish_color_valid_path --underline=single
 set --global fish_pager_color_background 
-set --global fish_pager_color_completion normal
-set --global fish_pager_color_description B3A06D
-set --global fish_pager_color_prefix normal --bold --underline=single
-set --global fish_pager_color_progress brwhite --bold --background=cyan
+set --global fish_pager_color_completion ebfafa
+set --global fish_pager_color_description 7081d0
+set --global fish_pager_color_prefix 04d1f9
+set --global fish_pager_color_progress 7081d0
 set --global fish_pager_color_secondary_background 
 set --global fish_pager_color_secondary_completion 
 set --global fish_pager_color_secondary_description 
 set --global fish_pager_color_secondary_prefix 
-set --global fish_pager_color_selected_background --background=brblack
+set --global fish_pager_color_selected_background --background=76639e
 set --global fish_pager_color_selected_completion 
 set --global fish_pager_color_selected_description 
 set --global fish_pager_color_selected_prefix 
