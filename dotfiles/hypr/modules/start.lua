581d9eb1ddd9622d2c1470c658b9ed0f2d549e87
Alexander Levy <ingalexanderlevy@gmail.com>
2026-05-27 00:42:22 -0400

    Added new wallpapers and changed to animated backend

--- dotfiles/hypr/modules/start.lua
+++ dotfiles/hypr/modules/start.lua
@@ -4,9 +4,10 @@
 hl.on("hyprland.start", function ()
     hl.exec_cmd("waybar")         -- Status Bar 
     hl.exec_cmd("swaync")         -- Notification center
-    hl.exec_cmd("hyprpaper")      -- Wallpaper 
+    -- hl.exec_cmd("hyprpaper")      -- Wallpaper 
     hl.exec_cmd("vicinae server") -- App launcher
     hl.exec_cmd("snappy-switcher --daemon") -- App switcher
     hl.exec_cmd("systemctl --user start hyprpolkitagent") -- Authorization engine
+    hl.exec_cmd("mpvpaper -p -o 'loop' ALL ~/Wallpapers/Undersea-room.mp4") -- Animated wallpaper 
 end)
 
