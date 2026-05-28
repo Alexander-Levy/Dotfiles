9f450af29df474e4a44fb9867f0e8509879c9fa0
Alexander Levy <ingalexanderlevy@gmail.com>
2026-05-28 19:38:51 -0400

    Changed default wallpapers

--- dotfiles/hypr/modules/start.lua
+++ dotfiles/hypr/modules/start.lua
@@ -8,6 +8,6 @@ hl.on("hyprland.start", function ()
     hl.exec_cmd("vicinae server") -- App launcher
     hl.exec_cmd("snappy-switcher --daemon") -- App switcher
     hl.exec_cmd("systemctl --user start hyprpolkitagent") -- Authorization engine
-    hl.exec_cmd("mpvpaper -p -o 'loop' ALL ~/Wallpapers/Undersea-room.mp4") -- Animated wallpaper 
+    hl.exec_cmd("mpvpaper -p -o 'loop' ALL ~/Wallpapers/Smoking-girl-city.mp4") -- Animated wallpaper 
 end)
 
